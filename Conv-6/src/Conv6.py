254a3c1ad9bf856366d597b4238d05ad1da8fd0b
Thytu <valentin.de-matos@epitech.eu>
2022-10-24 21:16:56 +0000

    fix(Conv-6): assure wieghts to be initialized to a non-zero value
    Signed-off-by: Thytu <valentin.de-matos@epitech.eu>

--- Conv-6/src/Conv6.py
+++ Conv-6/src/Conv6.py
@@ -50,6 +50,8 @@ class Conv6(nn.Module):
         def weights_init(m):
             if isinstance(m, nn.Conv2d) or isinstance(m, nn.Linear):
                 nn.init.xavier_normal_(m.weight.data)
+                m.weight.data[m.weight.data==0] = 0.005
+
                 nn.init.zeros_(m.bias.data)
 
         self.apply(weights_init)
